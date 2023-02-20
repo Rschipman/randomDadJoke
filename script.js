012ba78f7de4a964283b2fbc40e20bd4b6089f51
RChipman <rschipman@gmail.com>
2023-02-20 17:28:40 -0300

    bootstrap n stuff

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const jokes = document.querySelector("#jokes");
 
-const button = document.querySelector("button");
+const button = document.querySelector(".btn");
 
 const addNewJoke = async () => {
   const jokeText = await getDadJoke();
-  //   console.log(jokeText);
+
   const newLI = document.createElement("LI");
   newLI.append(jokeText);
   jokes.append(newLI);
 };
 
 const getDadJoke = async () => {
-  const config = { headers: { Accept: "application/json" } };
-  const res = await axios.get("https://icanhazdadjoke.com/", config);
-  return res.data.joke;
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "No Joke Available, sorry :((";
+  }
 };
 
 button.addEventListener("click", addNewJoke);
